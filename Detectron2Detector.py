82cbfcecea8c2be74ba5dc5edaab7b1dfdd64e36
565353780 <565353780@qq.com>
2022-01-25 11:32:23 +0800

    update code format

--- Detectron2Detector.py
+++ Detectron2Detector.py
@@ -56,7 +56,7 @@ class Detectron2Detector(object):
         result_dict["pred_masks"] = pred_masks
         return result_dict
 
-def demo():
+def demo_detect_image():
     model_path = "/home/chli/.ros/model_final_a3ec72.pkl"
     config_file = "/home/chli/.ros/configs/COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml"
 
@@ -74,6 +74,28 @@ def demo():
     cv2.waitKey(5000)
     return True
 
+def demo_detect_video():
+    model_path = "/home/chli/.ros/model_final_a3ec72.pkl"
+    config_file = "/home/chli/.ros/configs/COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml"
+
+    detectron2_detector = Detectron2Detector()
+    detectron2_detector.loadModel(model_path, config_file)
+
+    video_path = "/home/chli/videos/robot-1.mp4"
+    cap = cv2.VideoCapture(video_path)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        result_dict = detectron2_detector.detect_image(frame)
+        print(result_dict)
+
+        for box in result_dict["pred_boxes"].astype(int):
+            cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (0, 0, 255), 3)
+        cv2.imshow("result", frame)
+        cv2.waitKey(1)
+    return True
+
 if __name__ == "__main__":
-    demo()
+    demo_detect_video()
 
