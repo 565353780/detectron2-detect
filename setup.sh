c11a48d1f2671eac25d46cef92186328a3d3825d
chLi <565353780@qq.com>
2023-04-22 18:52:14 +0800

    upgrade pytorch and cuda version

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-pip install torch torchvision torchaudio \
-  --extra-index-url https://download.pytorch.org/whl/cu113
+pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 \
+  --extra-index-url https://download.pytorch.org/whl/cu116
 
 python -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
 
